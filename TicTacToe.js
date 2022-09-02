daa0da1e547ae46bedf95beff50f11cc63b935f8
Fajobi Feranmi Joseph <forjesus.fj@gmail.com>
2022-09-02 01:02:36 +0100

    error text for submit

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,4 +1,4 @@
-var aud = document.getElementById('startButto');
+var aud = document.getElementById('startButton');
 var xxL = localStorage.getItem("Player 1");
 var xxY = localStorage.getItem("Player 1");
 function playMusic() {
@@ -12,8 +12,14 @@ function stopMusic() {
     aud.currentTime = 0;
 }
 
-function startGm() {
-    window.location.href = "TicTacToe2.html";
+function submitNm() {
     localStorage.setItem("Player 1", firstPlayer.value);
     localStorage.setItem("Player 2", secondPlayer.value);
+
+    if(firstPlayer.value && secondPlayer.value){
+        window.location.href = "TicTacToe2.html";
+    } else{
+        dispText.innerHTML = "Please Enter Enter Your Names Before You Proceed"
+    }
 }
+
